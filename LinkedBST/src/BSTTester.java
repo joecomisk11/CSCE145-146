3a5ec5bb68615443327c8f35bb5edb3401b73434
joecomisk11 <70121407+joecomisk11@users.noreply.github.com>
2022-11-08 17:36:34 -0500

    Add files via upload

---
+++ LinkedBST/src/BSTTester.java
@@ -0,0 +1,17 @@
+
+public class BSTTester {
+
+	public static void main(String[] args) {
+		LinkedBST<Integer> iBST = new LinkedBST<Integer>();
+		iBST.add(8);
+		iBST.add(2);
+		iBST.add(12);
+		iBST.add(1);
+		iBST.add(4);
+		iBST.add(10);
+		iBST.add(16);
+		iBST.printInorder();
+
+	}
+
+}
